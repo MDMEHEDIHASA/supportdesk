42ce4a1478b8cf4a349981e80f6845a78886d993
Mehedi Hasan <mmehedihasan514@gmail.com>
2022-12-28 00:55:39 +0600

    add basic routes and controller

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
+const userRoutes = require('./routes/userRoutes')
+
 const app = express()
 const PORT = process.env.PORT || 5000
 
 app.get('/',(req,res,next)=>{
     res.send("Hello")
 })
+
+app.use('/api/users',userRoutes)
+
 
 app.listen(PORT,()=>console.log(`Server started on port ${PORT}`))
